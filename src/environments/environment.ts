1beb4060a58fcf3347ba859605086d3e7ad84c9e
Juliusouko <oukojulius224@gmail.com>
2025-05-24 15:39:09 +0300

    Adding Google Authentication

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,30 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
+    apiKey: 'AIzaSyCOMqwDcDwvHYLIPBvb28y-pCDFbtT3pTM',
+    authDomain: 'chat-app-24059.firebaseapp.com',
+    databaseURL: 'https://chat-app-24059.firebaseio.com',
+    projectId: 'chat-app-24059',
+    storageBucket: 'chat-app-24059.firebasestorage.app',
+    messagingSenderId: '273486990858',
+    appId: '1:273486990858:web:9f7d8057e1090c51b419f3',
+    measurementId: 'G-LCX2N64CXY',
   },
 };
+
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCOMqwDcDwvHYLIPBvb28y-pCDFbtT3pTM",
+//   authDomain: "chat-app-24059.firebaseapp.com",
+//   projectId: "chat-app-24059",
+//   storageBucket: "chat-app-24059.firebasestorage.app",
+//   messagingSenderId: "273486990858",
+//   appId: "1:273486990858:web:9f7d8057e1090c51b419f3",
+//   measurementId: "G-LCX2N64CXY"
+// };
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
